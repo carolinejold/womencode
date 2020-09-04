31ac2fd8ed0e018c394c625e7ec431011609975e
Caroline Old <carolinejold@gmail.com>
2020-09-04 20:10:35 +0200

    all working

--- src/components/resources/InterviewPractice.js
+++ src/components/resources/InterviewPractice.js
@@ -7,24 +7,15 @@ const InterviewPractice = () => {
       <Fade>
         <h1>Interview Practice</h1>
         <p>
-          It is a long established fact that a reader will be distracted by the
-          readable content of a page when looking at its layout. The point of
-          using Lorem Ipsum is that it has a more-or-less normal distribution of
-          letters, as opposed to using 'Content here, content here', making it
-          look like readable English. Many desktop publishing packages and web
-          page editors now use Lorem Ipsum as their default model text, and a
-          search for 'lorem ipsum' will uncover many web sites still in their
-          infancy. Various versions have evolved over the years, sometimes by
-          accident, sometimes on purpose (injected humour and the like). It is a
-          long established fact that a reader will be distracted by the readable
-          content of a page when looking at its layout. The point of using Lorem
-          Ipsum is that it has a more-or-less normal distribution of letters, as
-          opposed to using 'Content here, content here', making it look like
-          readable English. Many desktop publishing packages and web page
-          editors now use Lorem Ipsum as their default model text, and a search
-          for 'lorem ipsum' will uncover many web sites still in their infancy.
-          Various versions have evolved over the years, sometimes by accident,
-          sometimes on purpose (injected humour and the like).
+          <a
+            href={`https://dev.to/maxpou/typical-javascript-interview-exercises-explained`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Typical JavaScript Interview Questions Explained
+          </a>
+          <br />A simple list of beginners JavaScript interview questions
+          explained with clear code examples.
         </p>
       </Fade>
     </div>
